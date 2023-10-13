151167ae8beff7edfe3bc8285dcf6a0454b1b294
alican torun <alicantorun@alicans-air.localdomain>
2023-10-13 17:14:14 +0200

    turn off rule no-unescaped-entities

--- app/components/FAQs.tsx
+++ app/components/FAQs.tsx
@@ -66,8 +66,8 @@ export default function FAQs() {
   return (
     <div className="text-center max-w-5xl w-full items-center justify-between font-mono mb-12 lg:mb-24">
       <h1 className="mb-4 text-4xl font-extrabold">FAQs</h1>
-      {FAQ.map((item) => (
-        <AccordionItem title={item.title} content={item.content} />
+      {FAQ.map((item, index) => (
+        <AccordionItem key={index} title={item.title} content={item.content} />
       ))}
     </div>
   );
