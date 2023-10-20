3702b52616c9042a79aedd4cbd0b3dd857b4ddd7
alican torun <alicantorun@alicans-MacBook-Air.local>
2023-10-20 14:35:47 +0200

    fix transition bug

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <>
-      <div className="max-w-5xl w-full items-center justify-between flex mb-12 lg:mb-24">
+      <div className="z-50 max-w-5xl w-full items-center justify-between flex mb-12 lg:mb-24">
         <p className="">Bytehopper.AI</p>
         <div className=" justify-center items-center hidden lg:flex">
           <a className="mr-8" href="" target="_blank" rel="noopener noreferrer">
@@ -35,16 +35,16 @@ export default function Header() {
           </a>
           <ThemeToggleButton />
         </div>
-        <div className="lg:hidden">
+        <div className="lg:hidden z-50">
           <HamburgerMenu onClick={handleSetIsOpen} isOpen={isOpen} />
         </div>
       </div>
       <div
-        className={`absolute top-24 w-full left-0 transform transition-transform duration-300 ease-in-out overflow-hidden z-50 ${
+        className={`absolute top-24 w-full left-0 transform transition-transform duration-300 ease-in-out overflow-hidden  ${
           isOpen ? "translate-y-0 max-h-[300px]" : "-translate-y-full max-h-0"
         }`}
       >
-        <div className="dark:bg-black bg-white justify-center items-center flex flex-col opacity-90 pb-4">
+        <div className="dark:bg-black bg-white justify-center items-center flex flex-col opacity-90 pb-4 transition-colors duration-500">
           <a className="mb-4" href="" target="_blank" rel="noopener noreferrer">
             How we work
           </a>
