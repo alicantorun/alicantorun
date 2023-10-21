fd82cdf8cc6715876e5b606bc584657f593f4118
alican torun <alicantorun@alicans-air.localdomain>
2023-10-21 16:36:37 +0200

    fix mobile ontouch

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -9,25 +9,29 @@ export function Carousel() {
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [startX, setStartX] = useState<number>(0);
   const [scrollLeft, setScrollLeft] = useState<number>(0);
+  const lastTouchMove = useRef<number>(Date.now());
 
   const carouselRef = useRef<HTMLDivElement>(null);
 
-  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleStart = (clientX: number) => {
     setIsDragging(true);
-    setStartX(e.pageX);
+    setStartX(clientX);
     setScrollLeft(carouselRef.current!.scrollLeft);
   };
 
-  const handleMouseUp = () => {
-    setIsDragging(false);
+  const handleMove = (clientX: number) => {
+    if (!isDragging) return;
+    if (Date.now() - lastTouchMove.current < 20) return; // Throttle to 20ms
+    lastTouchMove.current = Date.now();
+
+    const x = clientX;
+    const distance = x - startX;
+    const speedMultiplier = window.innerWidth < 640 ? 6 : 3; // Faster scroll on smaller screens
+    carouselRef.current!.scrollLeft = scrollLeft - distance * speedMultiplier;
   };
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (!isDragging) return;
-    e.preventDefault();
-    const x = e.pageX;
-    const distance = x - startX;
-    carouselRef.current!.scrollLeft = scrollLeft - distance;
+  const handleEnd = () => {
+    setIsDragging(false);
   };
 
   const products: any[] = [
@@ -52,18 +56,26 @@ export function Carousel() {
     <div
       className=" w-full pb-8 overflow-x-hidden "
       ref={carouselRef}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
+      onMouseDown={(e) => handleStart(e.clientX)}
+      onMouseMove={(e) => handleMove(e.clientX)}
+      onMouseUp={handleEnd}
+      onMouseLeave={handleEnd}
+      onTouchStart={(e) => handleStart(e.touches[0].clientX)}
+      onTouchMove={(e) => {
+        e.preventDefault();
+        handleMove(e.touches[0].clientX);
+      }}
+      onTouchEnd={handleEnd}
     >
-      <ul className="flex animate-carousel gap-4">
+      <ul className="flex animate-carousel gap-4 ">
         {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
-            className="relative aspect-square h-[50vh] max-h-[325px] w-2/3 max-w-[475px] flex-none "
+            className="relative aspect-square max-h-[400px] w-2/3 max-w-[475px] flex-none "
           >
             <Image
-              className="relative object-cover h-full w-full rounded-xl border-black border-2 cursor-pointer"
+              draggable="false"
+              className=" relative object-cover h-full w-full rounded-xl border-black border-2 cursor-pointer"
               src={product.src}
               alt={product.alt}
               width="1024"
