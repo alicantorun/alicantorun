c81cfa6c92e1b8dc6593e9ed701a810770da235d
alican torun <alicantorun@alicans-air.localdomain>
2023-10-22 16:03:48 +0200

    add icon

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,22 +1,52 @@
 export default function BannerOne() {
   return (
-    <div className="text-center max-w-5xl w-full items-center justify-between   mb-12 lg:mb-24">
+    <div className="text-center max-w-5xl w-full items-center justify-between   mb-12 lg:mb-24 rounded-2xl bg-white shadow-lg p-8">
       <h1 className="mb-4 text-4xl font-extrabold">
         Websites, mobile apps, llms, chatbots & more.
       </h1>
-      <h3 className="text-xl">
-        Websites <br />
-        Mobile apps <br />
-        LLMs <br />
-        Chatbots <br />
-        SaaS <br />
-        Automations <br />
-        Zapier <br />
-        Authentication <br />
-        Forms <br />
-        Scalability <br />
-        Loadbalancing
-      </h3>
+
+      <ol className="text-lg text-left">
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Websites
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Mobile apps
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          LLMs
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Chatbots
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          SaaS
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Automations
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Zapier
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Authentication
+        </li>
+        <li className="mb-2 flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Forms
+        </li>
+        <li className="flex">
+          {/* <img className="mr-2" width={24} src="/checkmark.svg" /> */}
+          Scalability
+        </li>
+      </ol>
     </div>
   );
 }
