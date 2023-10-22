b1ec74ab3cca1a29d663e082aaba4baa4099778a
alican torun <alicantorun@alicans-air.localdomain>
2023-10-22 18:22:13 +0200

    bytehopper agency front page nice lookin boiii

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import { Laurel } from "./ui/Laurel";
 
 export default function Hero() {
   return (
-    <div className="mt-36 text-center max-w-5xl w-full items-center justify-between mb-24 p-4">
+    <div className="lg:mt-36 text-center max-w-5xl w-full items-center justify-between mb-24 p-4">
       <h1 className="text-left text-5xl lg:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-black dark:from-purple-600 dark:to-white">
         Software & AI Automation,
       </h1>
@@ -14,12 +14,12 @@ export default function Hero() {
         solutions that enhance efficiency. For companies utilizing +$10,000/mo
         in software tools.
       </h3>
-      <h3 className="text-left mb-16 text-xl">
+      <h3 className="text-left mb-8 text-xl">
         By people who built & sold SaaS companies, through SEO.
       </h3>
-      <div className="mb-8 flex flex-col lg:block">
+      <div className="mb-8 lg:p-4 flex flex-col lg:block">
         <a
-          className=" lg:mr-4 text-xl px-8 py-4 rounded-xl bg-black text-white mb-4 lg:mb-0"
+          className="lg:mr-4 text-xl px-8 py-4 rounded-xl bg-black hover:bg-slate-700 text-white mb-4 lg:mb-0 box-border"
           href="https://calendly.com/alicantorun/30min"
           target="_blank"
           rel="noopener noreferrer"
@@ -27,17 +27,19 @@ export default function Hero() {
           Schedule a call 📅
         </a>
         <a
-          className=" text-xl px-8 py-4 rounded-xl bg-black text-white"
+          className="text-xl px-8 py-4 rounded-xl bg-black hover:bg-slate-700 text-white box-border"
           href="#membership-levels"
         >
           See plans 🏷️
         </a>
       </div>
-      <Laurel>
-        <h3 className="text-left text-2xl font-bold">Best Agency</h3>
-      </Laurel>
+      <div className="mb-4">
+        <Laurel>
+          <h3 className="text-left text-xl font-bold">Agency like no others</h3>
+        </Laurel>
+      </div>
       <h3 className="">
-        Software you'll <span className="text-2xl text-red-600">❤️</span>,
+        AI & Software you'll <span className="text-2xl text-red-600">❤️</span>,
         guaranteed.
       </h3>
     </div>
