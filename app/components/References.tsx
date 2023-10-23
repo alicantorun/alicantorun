3e5d2c2198282fb23a1711becefc3c2c46dd7b17
alican torun <alicantorun@alicans-air.localdomain>
2023-10-23 20:10:43 +0200

    fix references

--- app/components/References.tsx
+++ app/components/References.tsx
@@ -1,61 +1,46 @@
+import React from "react";
+
 export default function BannerOne() {
   return (
     <div className="text-center max-w-5xl w-full mb-12 lg:mb-24 px-4">
       <h1 className="mb-4 text-4xl font-extrabold w-full">References</h1>
-      <div className="flex items-center justify-between">
-        <a
+      <div className="grid lg:grid-cols-4 grid-cols-2 gap-4">
+        <ReferenceLink
           href="https://klepierre.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ width: "20%", height: "100px" }}
-        >
-          <img
-            src="/klepierre.png"
-            alt=""
-            className="rounded-lg flex-none mx-1 object-contain"
-            style={{ width: "100%", height: "100px" }}
-          />
-        </a>
-        <a
+          imgSrc="/klepierre.png"
+          alt=""
+        />
+        <ReferenceLink
           href="https://cirplus.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ width: "20%", height: "100px" }}
-        >
-          <img
-            src="/cirplus.svg"
-            alt=""
-            className="rounded-lg flex-none mx-1 object-contain"
-            style={{ width: "100%", height: "100px" }}
-          />
-        </a>
-        <a
-          href="https://parloa.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ width: "20%", height: "100px" }}
-        >
-          <img
-            src="/parloa.webp"
-            alt=""
-            className="rounded-lg flex-none mx-1 object-contain"
-            style={{ width: "100%", height: "100px" }}
-          />
-        </a>
-        <a
-          href="https://antler.co"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ width: "20%", height: "100px" }}
-        >
-          <img
-            src="/antler.png"
-            alt=""
-            className="rounded-lg flex-none mx-1 object-contain"
-            style={{ width: "100%", height: "100px" }}
-          />
-        </a>
+          imgSrc="/cirplus.svg"
+          alt=""
+        />
+        <ReferenceLink href="https://parloa.com" imgSrc="/parloa.webp" alt="" />
+        <ReferenceLink href="https://antler.co" imgSrc="/antler.png" alt="" />
       </div>
     </div>
   );
 }
+
+interface ReferenceLinkProps {
+  href: string;
+  imgSrc: string;
+  alt: string;
+}
+
+const ReferenceLink: React.FC<ReferenceLinkProps> = ({ href, imgSrc, alt }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="h-24 p-4 w-full flex items-center justify-center"
+    >
+      <img
+        src={imgSrc}
+        alt={alt}
+        className="rounded-lg object-contain h-full w-full"
+      />
+    </a>
+  );
+};
